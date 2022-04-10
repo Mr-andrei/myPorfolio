c370a7e4c99af276319e941b9747aaeb1b89fce5
Mr-andrei <91520213+Mr-andrei@users.noreply.github.com>
2022-04-10 16:44:27 +0300

    new commit

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,15 +1,29 @@
 import style from './style.module.css'
 import Slide from 'react-reveal/Slide';
-import { Link, animateScroll as scroll } from "react-scroll";
+import {Link, animateScroll as scroll} from "react-scroll";
+import {useState} from "react";
 
 const NavBar = () => {
+
+    const [openMenu,  setOpenMenu] = useState(false)
+
     return (
         <div className={style.mainWrapper}>
-            <div className={style.container}>
+             <span className={style.hamburger} onClick={()=> setOpenMenu (!openMenu)}>
+                        <hr className={style.line1}></hr>
+                        <hr className={style.line2}></hr>
+                        <hr className={style.line3}></hr>
+               </span>
+            <div className={openMenu ?   style.containerOpen : style.container}>
+                <span
+                    className={style.close}
+                    onClick={()=> setOpenMenu (false)}
+                ></span>
                 <Slide right cascade>
-                <nav className={style.nav_block}>
-                    <span className={style.text_linc}>
+                    <nav className={style.nav_block}>
+                    <span className={style.text_linc} >
                           <Link
+                              onClick={()=> setOpenMenu (false)}
                               activeClass={style.active}
                               to="aboutMe"
                               spy={true}
@@ -18,8 +32,9 @@ const NavBar = () => {
                               duration={500}
                           > About me </Link>
                     </span>
-                    <span className={style.text_linc}>
+                        <span className={style.text_linc} >
                          <Link
+                             onClick={()=> setOpenMenu (false)}
                              activeClass={style.active}
                              to="skills"
                              spy={true}
@@ -28,8 +43,9 @@ const NavBar = () => {
                              duration={500}
                          > My skills </Link>
                     </span>
-                    <span className={style.text_linc}>
+                        <span className={style.text_linc}>
                         <Link
+                            onClick={()=> setOpenMenu (false)}
                             activeClass={style.active}
                             to="work"
                             spy={true}
@@ -37,8 +53,9 @@ const NavBar = () => {
                             offset={0}
                             duration={500}
                         > My works </Link></span>
-                    <span className={style.text_linc}>
+                        <span className={style.text_linc}>
                         <Link
+                            onClick={()=> setOpenMenu (false)}
                             activeClass={style.active}
                             to="contact"
                             spy={true}
@@ -46,7 +63,7 @@ const NavBar = () => {
                             offset={0}
                             duration={500}
                         > Contacts </Link></span>
-                </nav>
+                    </nav>
                 </Slide>
             </div>
         </div>
